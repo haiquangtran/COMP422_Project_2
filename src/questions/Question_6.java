f3e921a24a118c96b6d10788fea7ead7194409d6
haiquangtran <hai.quang.tran@gmail.com>
2015-09-27 07:11:02 +1300

    added main class for q6.

---
+++ src/questions/Question_6.java
@@ -0,0 +1,22 @@
+package questions;
+
+public class Question_6 {
+
+	/**
+	 * Choose two single feature ranking algorithms (e.g. Information Gain and Chi-square) and
+	 * apply them to Wisconsin Breast Cancer and Sonar data sets. Choose a classifier (e.g na¨ıve
+	 * Bayes) and a number n representing the desired number of selected features (e.g. 5). Do the
+	 * following:
+	 * 1. Compare the performance of the classifier using all the features vs n top ranked features
+	 * from each method. Analyse your results.
+	 * 2. Use a wrapper approach to select n features and then measure the performance of the
+	 * classifier using the selected features. Compare the result with the filter (single-ranking)
+	 * approach and analyse your findings.
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
