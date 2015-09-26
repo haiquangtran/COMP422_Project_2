4bb0aeecaacaa90960092c0b35a269c10673823e
haiquangtran <hai.quang.tran@gmail.com>
2015-09-27 06:52:29 +1300

    added main class for q5.

---
+++ src/questions/Question_5.java
@@ -0,0 +1,23 @@
+package questions;
+
+public class Question_5 {
+
+	/**
+	 * First apply the na¨ıve Bayes and Decision Tree (C4.5) classifiers to the Balance Scale and
+	 * Wine Recognition datasets and measure the classification performance using 10-fold crossvalidation.
+	 * Then develop a GP-based feature construction algorithm to construct features for
+	 * the two problems. Feed the constructed features to these classifiers and report changes in
+	 * their performance. Consider the following points:
+	 * • Design an appropriate fitness function.
+	 * • Use the best individual (out of one or several runs) to transform the data. Include the
+	 * program (the best individual) in your report (in the form of Lisp code, tree, ...).
+	 * • Report and analyse changes in performance.
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
