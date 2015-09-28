69d31fa2ca86392798cde6cfe0791ad6d44426b5
haiquangtran <hai.quang.tran@gmail.com>
2015-09-28 18:54:22 +1300

    settings for symbolic_regression.conf. cleaned .conf

--- src/questions/Question_3.java
+++ src/questions/Question_3.java
@@ -22,8 +22,8 @@ public class Question_3 {
 	 */
 	public static void main(String[] args) {
 		try {
-			int min = -10;
-			int max = 10;
+			int min = -100;
+			int max = 100;
 			int numOfExamples = 1000;
 			String fileName = FileLoader.getFilePath("symbolic_dataset");
 			// Generate data set first
