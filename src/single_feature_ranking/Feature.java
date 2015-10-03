4bac0bfcd7252eb0b3c0f8f240fcff2766bfc230
haiquangtran <hai.quang.tran@gmail.com>
2015-10-04 02:47:03 +1300

    remove pearson code from feature class.

--- src/single_feature_ranking/Feature.java
+++ src/single_feature_ranking/Feature.java
@@ -3,41 +3,10 @@ package single_feature_ranking;
 public class Feature {
 	private double[] features;
 	private int featureClass;
-	// Testing purposes
-	private double pearsonScore;
 
 	public Feature(double[] features, int featureClass) {
 		this.features = features;
 		this.featureClass = featureClass;
-	}
-
-	public double comparePearsonCorrelationTo(Feature other) {
-		double x = 0;
-		double y = 0;
-		int n = features.length;
-		double r = 0;
-
-		// Find totals for xy, x and y
-		for (int i = 0; i < n; i++) {
-			x += (features[i]);
-			y += (other.getFeatures()[i]);
-		}
-
-		r = ((n * (x*y) - (x)*(y)) / (Math.sqrt( ((n*Math.pow(x, 2))-Math.pow(x, 2)) * ((n*Math.pow(y, 2))-Math.pow(y, 2)) )));
-		this.pearsonScore = r;
-
-//		System.out.println("R " + r);
-		// r in [-1, 1]
-		return r;
-	}
-
-	/**
-	 * For testing purposes.
-	 *
-	 * @return
-	 */
-	public double getPearsonScore() {
-		return pearsonScore;
 	}
 
 	public double[] getFeatures() {
