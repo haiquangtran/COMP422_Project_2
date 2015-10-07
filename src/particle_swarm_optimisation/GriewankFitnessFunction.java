02b3ddae432b842c4a67edd2b7425c8b845d1f81
haiquangtran <hai.quang.tran@gmail.com>
2015-10-07 16:27:28 +1300

    implemented rosenbrock and griewanks functions for PSO

--- src/particle_swarm_optimisation/GriewankFitnessFunction.java
+++ src/particle_swarm_optimisation/GriewankFitnessFunction.java
@@ -7,29 +7,18 @@ public class GriewankFitnessFunction extends Problem {
 
 	@Override
 	public double fitness(List<Double> position) {
-		//		double sum1 = 0;
-		//        double sum2 = 0;
-		//        double fitness = 0;
-		//        for (int i = 0; i < position.size(); ++i) {
-		//            sum1 += position.get(i) * position.get(i);
-		//            sum2 += Math.cos(2 * Math.PI * position.get(i));
-		//        }
-		//        //m_dFitness
-		//        fitness = -20 * Math.exp(-0.2 * Math.sqrt((1.0 / position.size()) * sum1)) - Math.exp((1.0 / position.size()) * sum2) + 20 + Math.E;
-		//
-		//        return fitness;
-		// TODO Auto-generated method stub
-		return 0;
+		// Use griwank's function as the fitness function.
+		return griewanksFunction(position);
 	}
 
 	/**
 	 *  Griewank's function
 	 */
-	public double griewanksFunction(double dimension[]) {
+	public double griewanksFunction(List<Double> position) {
 		double griewanks = 0;
-		for (int i = 0; i < dimension.length; i++) {
-			if (dimension[i] <= 30 && dimension[i] >= 30) {
-				griewanks += (Math.pow(dimension[i], 2)/4000) + getGriewanks(dimension);
+		for (int i = 0; i < position.size(); i++) {
+			if (position.get(i) <= 30 && position.get(i) >= 30) {
+				griewanks += (Math.pow(position.get(i), 2)/4000) + getGriewanks(position);
 			}
 		}
 
@@ -40,10 +29,10 @@ public class GriewankFitnessFunction extends Problem {
 	 *  Griewank's function helper.
 	 *  Return the second part of the equation.
 	 */
-	private double getGriewanks(double dimension[]) {
+	private double getGriewanks(List<Double> position) {
 		double griewanks = 0;
-		for (int i = 0; i < dimension.length; i++) {
-			griewanks = Math.cos(dimension[i]/Math.sqrt(i)) + 1;
+		for (int i = 0; i < position.size(); i++) {
+			griewanks = Math.cos(position.get(i)/Math.sqrt(i)) + 1;
 		}
 		return griewanks;
 	}
--- src/particle_swarm_optimisation/RosenbrockFitnessFunction.java
+++ src/particle_swarm_optimisation/RosenbrockFitnessFunction.java
@@ -2,26 +2,24 @@ package particle_swarm_optimisation;
 
 import java.util.List;
 
-import net.sourceforge.jswarm_pso.FitnessFunction;
-
 public class RosenbrockFitnessFunction extends Problem {
 
 	@Override
 	public double fitness(List<Double> position) {
-		// TODO Auto-generated method stub
-		return 0;
+		// Use rosenbrock's function as the fitness function.
+		return rosenbrocksFunction(position);
 	}
 
 	/**
 	 * Rosenbrock's function
 	 *
 	 */
-	public double rosenbrocksFunction(double dimension[]) {
+	public double rosenbrocksFunction(List<Double> position) {
 		double rosenbrocks = 0;
 		//TODO: Check why D-1 (Check the loop)
-		for (int i = 0; i < dimension.length; i++) {
-			if (dimension[i] <= 30 && dimension[i] >= -30) {
-				rosenbrocks += 100 * Math.pow((Math.pow(dimension[i], 2)-(dimension[i]+1)), 2) + Math.pow((dimension[i]-1), 2);
+		for (int i = 0; i < position.size()-1; i++) {
+			if (position.get(i) <= 30 && position.get(i) >= -30) {
+				rosenbrocks += 100 * Math.pow((Math.pow(position.get(i), 2)-(position.get(i+1))), 2) + Math.pow((position.get(i)-1), 2);
 			}
 		}
 
